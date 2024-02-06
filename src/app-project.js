f871add237c06f5bb39c0afc8b20a36ed5f51766
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-02-06 01:21:54 +0000

    project done

---
+++ src/app-project.js
@@ -0,0 +1,15 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+let randomDomain = ['.com', '.net', '.us', '.io'];
+
+for (let i = 0; i <= pronoun.length -1; i++){
+    for (let j = 0; j <= adj.length -1; j++){
+        for (let y = 0; y <= noun.length -1; y++){
+            let randomIndex = Math.floor(Math.random() * randomDomain.length);
+            console.log(pronoun[i] + adj[j] + noun[y] + randomDomain[randomIndex]);
+        }
+    }
+}
+console.log("Any string");
