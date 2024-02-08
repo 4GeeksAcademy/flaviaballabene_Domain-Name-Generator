43cdc83569ffa237d8a69d9dd2bff355a9f5e310
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-02-08 01:49:49 +0000

    clean code

--- app-project.js
+++ app-project.js
@@ -1,14 +1,14 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+let pronoun = ["the", "our"];
+let adjective = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-let randomDomain = ['.com', '.net', '.us', '.io'];
+let domains = [".com", ".net", ".us", ".io"];
 
-for (let i = 0; i <= pronoun.length -1; i++){
-    for (let j = 0; j <= adj.length -1; j++){
-        for (let y = 0; y <= noun.length -1; y++){
-            let randomIndex = Math.floor(Math.random() * randomDomain.length);
-            console.log(pronoun[i] + adj[j] + noun[y] + randomDomain[randomIndex]);
-        }
+for (let i = 0; i <= pronoun.length - 1; i++) {
+  for (let j = 0; j <= adjective.length - 1; j++) {
+    for (let y = 0; y <= noun.length - 1; y++) {
+      let randomIndexForDomain = Math.floor(Math.random() * domains.length);
+      console.log(pronoun[i] + adjective[j] + noun[y] + domains[randomIndexForDomain]);
     }
+  }
 }
